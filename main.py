5b90d282466d999e664372eab3c241dbbd516f1e
tmharwood <105583481+tmharwood@users.noreply.github.com>
2022-08-07 20:56:30 -0700

    added b64/sha256/md5

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 
 from tkinter import *
 from tkinter import ttk
+import time, hashlib, base64
 
 # Display the changelog
 def chng_click():
@@ -11,6 +12,10 @@ def chng_click():
     chnglog.title("Encoder Changelog")
     lbl2 = Label(chnglog, text="7/11/2022: \nCreated the program! Hello World!")
     lbl3 = Label(chnglog, text="7/23/2022: \nCreated microservice!")
+    lbl4 = Label(chnglog, text="8/6/2022: \n Added ability to use partner's microservice.")
+    lbl5 = Label(chnglog, text="8/7/2022: \n Added b64, sha256, md5, scrollbar.")
+    lbl5.pack()
+    lbl4.pack()
     lbl3.pack()
     lbl2.pack()
     chnglog.mainloop() 
@@ -21,14 +26,37 @@ def clear_input():
 
 # Function for encoding
 def encode_fun():
+
     # clear current contents
     output_box.delete('1.0', END)
-    output_box.insert('1.0', input_box.get('1.0', END))
-    # Remove the automatically inserted newline. Code modified from:
-    # https://stackoverflow.com/questions/48220788/how-can-i-remove-newline-character-n-at-the-end-of-text-widget
-    # Accessed 7/11/22
-    if output_box.get('end-1c', 'end') == '\n':
-        output_box.delete('end-1c', 'end')
+
+    ptext = input_box.get('1.0', END)
+    ptext = ptext.strip('\n')
+    ptext = ptext.encode('ascii')
+    if select_encode.get() == "Base64":
+        b64_enc(ptext)
+    elif select_encode.get() == "MD5":
+        md5_enc(ptext)
+    elif select_encode.get() == "Sha256":
+        sha256_enc(ptext)
+
+
+# Encode to base 64
+def b64_enc(ptext):
+    output = base64.b64encode(ptext)
+    output_box.insert('1.0', output)
+
+# Encode to md5
+def md5_enc(ptext):
+    output = hashlib.md5()
+    output.update(ptext)
+    output_box.insert('1.0', output.hexdigest())
+
+# Encode to SHA256
+def sha256_enc(ptext):
+    output = hashlib.sha256()
+    output.update(ptext)
+    output_box.insert('1.0', output.hexdigest())
 
 # Function to copy contents of output box
 def copy_fun():
@@ -36,9 +64,20 @@ def copy_fun():
     main.clipboard_append(output_box.get('1.0', END))
     main.update()
 
+# Export output box to file
 def export_fun():
     with open('encoded.txt', 'w', encoding='utf-8') as f:
         f.write(output_box.get('1.0', END))
+
+# Use partner's Microservice to get input
+def microservice():
+    clear_input()
+    with open("service.txt", "w") as f:
+        f.write("Start")
+    time.sleep(10)
+    with open("userOutput.txt", "r", encoding="utf-8") as f2:
+        mes = f2.read()
+        input_box.insert("1.0", mes)
 
 
 # Set up the main window
@@ -61,7 +100,7 @@ input_box.grid(column=1, row=1, columnspan=3)
 # Create combobox
 selection = StringVar()
 select_encode = ttk.Combobox(main, width=25, textvariable=selection)
-select_encode['values'] = ('test1', 'test2', 'test3')
+select_encode['values'] = ('Base64', 'MD5', 'Sha256')
 select_encode['state'] = 'readonly'
 select_encode.grid(column=2, row=2)
 select_encode.set('Select Encoding Method')
@@ -85,6 +124,20 @@ cpy_button.grid(column=1, row=4)
 # Export Button
 export_button = Button(main, text="Export Output", command=export_fun, padx=10, pady=10)
 export_button.grid(column=3, row=4)
+
+# Microservice Button
+micro_button = Button(main, text="Run Microservice", command=microservice, padx=10, pady=10)
+micro_button.grid(column=3, row=0)
+
+# Input scrollbar
+input_scroll = ttk.Scrollbar(main, orient='vertical', command=input_box.yview)
+input_scroll.grid(row=1, column=4, sticky=NS)
+input_box['yscrollcommand'] = input_scroll.set
+
+# Output scrollbar
+output_scroll = ttk.Scrollbar(main, orient='vertical', command=output_box.yview)
+output_scroll.grid(row=3, column=4, sticky=NS)
+output_box['yscrollcommand'] = output_scroll.set
 
 # Run the window!
 main.mainloop()
